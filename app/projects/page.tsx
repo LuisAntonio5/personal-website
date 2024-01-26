22e7005c24b9ee12eef687f75cf3a29f650b0dd4
Luís António <luis.antonio.lima.silva@gmail.com>
2024-01-26 23:46:59 +0000

    Add the projects route and page

---
+++ app/projects/page.tsx
@@ -0,0 +1,10 @@
+import Navbar from '@/components/navbar';
+
+export default function Projects() {
+  return (
+    <div>
+        <Navbar/>
+        <h2 className='text-center'>Under construction</h2>
+    </div>
+  )
+}
