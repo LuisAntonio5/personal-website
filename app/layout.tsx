ee3289bdc06f65920aac373db572fc54e9a10ac7
Luís António <luis.antonio.lima.silva@gmail.com>
2024-01-26 23:50:51 +0000

    Prevent bug where FA icons were rendered before the CSS was loaded

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../styles/globals.css";
+// The following import prevents a Font Awesome icon server-side rendering bug,
+// where the icons are being rendered before the CSS is loaded:
+import '@fortawesome/fontawesome-svg-core/styles.css';
+// Prevent fontawesome from adding its CSS since we did it manually above:
+import { config } from '@fortawesome/fontawesome-svg-core';
+config.autoAddCss = false; /* eslint-disable import/first */
 
 const inter = Inter({ subsets: ["latin"] });
 
